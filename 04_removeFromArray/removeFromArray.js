4fc588df73fd01723898c011b2991e7bf8835b2d
Darien Lin <darienlin87@gmail.com>
2023-09-07 00:17:08 -0700

    finsihed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removed) {
+    for(let i = 0; i < removed.length; i++){
+        let indexRemoved = array.indexOf(removed[i])
+        if(indexRemoved > -1)
+        array.splice(indexRemoved,1)
+    }
+    return array
 };
 
 // Do not edit below this line
