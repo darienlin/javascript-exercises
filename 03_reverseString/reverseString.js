7f10ddb93a74d60321ad67f56bc86fbea644b004
Darien Lin <darienlin87@gmail.com>
2023-09-06 23:59:31 -0700

    solved reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let word = ""
+    for(let i = string.length; i >= 0; i--){
+        word = word + string.slice(i - 1, i)
+    }
+    return word
 };
 
 // Do not edit below this line
